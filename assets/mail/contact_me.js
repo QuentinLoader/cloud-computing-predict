8e65c379baac7ac0984fc3a99969e44c41a1a785
Quentin Loader <quentin.loader@gmail.com>
2022-01-03 11:25:39 +0200

    Commit

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = { name: name, phone: phone, email: email, message: message }
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{https://m7ud6qd7k4.execute-api.eu-west-1.amazonaws.com}",
+                url: "{ https://y3dq9y5i4b.execute-api.eu-west-1.amazonaws.com/default/AWS_Lambda_Function_Predict}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
